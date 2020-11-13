6e8fef758d14cf15c88439bf86658b728318b498
amansdpr <amansdpr@gmail.com>
2020-11-14 02:47:05 +0600

    sticky nav

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,1 +1,27 @@
-main-script.js
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+      
+
+        /*  -------------------------------------------------------------------------  *
+         *                   02. change Menu background on scroll donw                 *
+         *  -------------------------------------------------------------------------  */
+          $(window).on('scroll', function () {
+              var menu_area = $('.navbar-sticky');
+              if ($(window).scrollTop() > 200) {
+                  menu_area.addClass('sticky-menu');
+              } else {
+                  menu_area.removeClass('sticky-menu');
+              }
+          });
+
+
+          $("#search-toggle").click(function(e){
+            e.preventDefault();
+            $(".search-form").toggleClass('show');
+          });
+
+    });
+
+})(jQuery);
